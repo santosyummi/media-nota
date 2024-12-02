2cedb0c10fdfe0183ead3423fa952d2aae4dfb6a
santosyummi <141366425+santosyummi@users.noreply.github.com>
2024-12-02 11:32:25 -0300

    ok

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementeByld('formulario').addEventListener('submit', function(event){
+event.preventDefault();
+//obter as notas inseridas pelo usuário
+let nota1=parseFloat(document.getElementeByld ('nota1').value);
+let nota2=parseFloat(document.getElementeByld ('nota2').value);
+let nota3=parseFloat(document.getElementeByld ('nota3').value);
+
+//calcular a média
+let media=(nota1+nota2+nota3)/3;
+
+//exibir a mesagem de resultado
+let resultadoDiv=document.getElementeByld ('resultado')
+if(media>=6.0){
+    resultadoDiv.innerHTML =  `<p class="aprovdo">Aprovado! Sua média foi: ${media.toFixed(2)}</p>`
+}
+
+}
